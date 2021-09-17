3a3c39fb4126df112795ad5e79c555449a9a0ee7
FinEko05 <64735335+FinEko05@users.noreply.github.com>
2021-09-17 22:00:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,22 +3,9 @@
 #it's better to write import in one file then import that file to every file rather than importing everything to every file.
 
 from os import system
-import discord, os, youtube_dl
-from currency_converter import CurrencyConverter
+import discord, os
 from discord.ext import commands
 
 prefix = "." # prefix before commands
 
-embed_color = discord.Color.red() # Embed color for messages and such
-game_playing = "Wee woo Wee Woo" # Bot's game activity
-
-
-
-
-
-#
-# Bot_token = "" <- your token this is only for repl it
-#
-bot_token = os.environ['Bot_token_key_thingy'] 
-#
-#
+bot_token = ""  # <- Your Bot Token
