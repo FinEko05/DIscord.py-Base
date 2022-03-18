4e0e446779d3b1c5118a7eab0c1c5b2078099aff
FinEko05 <64735335+FinEko05@users.noreply.github.com>
2022-03-18 08:42:56 +0200

    Update main.py
    .

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-from config import *
+import os
+from discord.ext import commands
+
 bot = commands.Bot(command_prefix='.', help_command=None)
 
 for filename in os.listdir('./cogs'):
   if filename.endswith('.py'):
     try:
       bot.load_extension(f'cogs.{filename[:-3]}')
-      print(f"{filename} succesfully loaded")
-    except:
-      print(f"Exception in {filename} unable to load_extension()")
+      print(f"{filename} loaded")
+    except Exception as e:
+      print(f"Exception in {filename}\n\n {e}")
   else:
     if filename != "__pycache__":
       print(f'{filename[:-3]} Not an .py file')
 
 @bot.event
 async def on_ready():
-    print(f"\nBot {bot.user} Online!")
-  
+  print(f"\n{bot.user} Online!")
+
 bot.run(bot_token)
