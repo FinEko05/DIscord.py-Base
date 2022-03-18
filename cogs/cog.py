121681a1c688a407053eeba7ba3508b671aa4216
FinEko05 <64735335+FinEko05@users.noreply.github.com>
2022-03-18 08:44:53 +0200

    Update cog.py

--- cogs/cog.py
+++ cogs/cog.py
@@ -2,13 +2,13 @@ from config import *
 
 #example cog
 
-class Cog(commands.Cog):
+class example_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
     @commands.command()
     async def test(self, ctx):
-        await ctx.send("Test!")
+        await ctx.send("Hello World!")
 
 def setup(bot):
-    bot.add_cog(Cog(bot))
+    bot.add_cog(example_cog(bot))
